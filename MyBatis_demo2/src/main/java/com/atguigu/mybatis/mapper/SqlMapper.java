4841621b43d53acd339fa7786ef3e90a83b624c8
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-04-02 07:47:42 +0800

    add

--- MyBatis_demo2/src/main/java/com/atguigu/mybatis/mapper/SqlMapper.java
+++ MyBatis_demo2/src/main/java/com/atguigu/mybatis/mapper/SqlMapper.java
@@ -8,4 +8,10 @@ import java.util.List;
 public interface SqlMapper {
 
     List<User> getUserByLike(@Param("username") String username);
+
+    int deleteMore(@Param("ids") String ids);
+
+    List<User> getUserByTableName(@Param("tableName") String tableNem);
+
+    void insertUser(User user);
 }
--- MyBatis_demo2/src/test/java/com/atguigu/mybatis/test/SqlMapperTest.java
+++ MyBatis_demo2/src/test/java/com/atguigu/mybatis/test/SqlMapperTest.java
@@ -6,6 +6,7 @@ import com.atguigu.mybatis.utils.SqlSessionUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.sql.*;
 import java.util.List;
 
 public class SqlMapperTest {
@@ -17,5 +18,42 @@ public class SqlMapperTest {
         List<User>  list = sqlMapper.getUserByLike("a");
 
         System.out.println(list);
+    }
+
+    @Test
+    public void testDeleteMore(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SqlMapper sqlMapper = sqlSession.getMapper(SqlMapper.class);
+       int result =  sqlMapper.deleteMore("1,2,3");
+        System.out.println(result);
+
+    }
+
+    @Test
+    public void testGetUserByTableName(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SqlMapper sqlMapper = sqlSession.getMapper(SqlMapper.class);
+        List<User> users = sqlMapper.getUserByTableName("t_user");
+        System.out.println(users);
+
+    }
+
+    @Test
+    public void testJdbc() throws Exception {
+        Class.forName("");
+        Connection connection = DriverManager.getConnection("","","");
+        PreparedStatement ps = connection.prepareStatement("insert", Statement.RETURN_GENERATED_KEYS);
+        ps.executeUpdate();
+        ResultSet resultSet = ps.getGeneratedKeys();
+    }
+
+    @Test
+    public void testInsertUser(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SqlMapper sqlMapper = sqlSession.getMapper(SqlMapper.class);
+        User user = new User(null,"王五","123",23,"男","123@163.com");
+        sqlMapper.insertUser(user);
+        System.out.println(user);
+
     }
 }
