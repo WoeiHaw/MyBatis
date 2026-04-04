24d9a35f61896fc39729999f802b92a75a5e36cf
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-04-04 15:56:26 +0800

    add join table using mybatis

---
+++ MyBatis_demo3/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
@@ -0,0 +1,13 @@
+package com.atguigu.mybatis.mapper;
+
+import com.atguigu.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface EmpMapper {
+
+    List<Emp> getAllEmp();
+
+    Emp getEmpAndDept(@Param("eid") Integer eid );
+}
---
+++ MyBatis_demo3/src/test/java/ResultMapTest.java
@@ -0,0 +1,26 @@
+import com.atguigu.mybatis.mapper.EmpMapper;
+import com.atguigu.mybatis.pojo.Emp;
+import com.atguigu.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ResultMapTest {
+
+    @Test
+    public void testGetAllEmp(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        List<Emp> emps = mapper.getAllEmp();
+        emps.forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetEmpAndDept(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.getEmpAndDept(1);
+        System.out.println(emp);
+    };
+}
