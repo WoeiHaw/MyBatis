262dc200d1a543f42ede674459b0863463e3abd2
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-04-19 12:14:01 +0800

    add code for dynamic query

--- MyBatis_demo3/src/main/java/com/atguigu/mybatis/mapper/DynamicSqlMapper.java
+++ MyBatis_demo3/src/main/java/com/atguigu/mybatis/mapper/DynamicSqlMapper.java
@@ -1,6 +1,7 @@
 package com.atguigu.mybatis.mapper;
 
 import com.atguigu.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -9,4 +10,8 @@ public interface DynamicSqlMapper {
     List<Emp> getEmpByCondition(Emp emp);
 
     List<Emp> getEmpByChoose(Emp emp);
+
+    int deleteMoreByArray(@Param("eids") Integer[] eids);
+
+    int insertMoreByList(@Param("emps")List<Emp> emps);
 }
--- MyBatis_demo3/src/test/java/com/atguigu/mybatis/test/DynamicSQLMapperTest.java
+++ MyBatis_demo3/src/test/java/com/atguigu/mybatis/test/DynamicSQLMapperTest.java
@@ -6,6 +6,7 @@ import com.atguigu.mybatis.utils.SqlSessionUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class DynamicSQLMapperTest {
@@ -24,5 +25,26 @@ public class DynamicSQLMapperTest {
         DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
         List<Emp> list = mapper.getEmpByChoose(new Emp(null,"张三",23,"男","123@qq.com"));
         System.out.println(list);
+    }
+
+    @Test
+    public  void testDeleteMoreByArray(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+        int result = mapper.deleteMoreByArray(new Integer[]{6,7,8});
+        System.out.println(result);
+    }
+
+    @Test
+    public  void testInserMoreByList(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+        Emp emp1 = new Emp(null,"a",23,"男","123@qq.com");
+        Emp emp2 = new Emp(null,"a",23,"男","123@qq.com");
+        Emp emp3 = new Emp(null,"a",23,"男","123@qq.com");
+        List<Emp> list = Arrays.asList(emp1,emp2,emp3);
+        System.out.println(mapper.insertMoreByList(list));
+
+
     }
 }
